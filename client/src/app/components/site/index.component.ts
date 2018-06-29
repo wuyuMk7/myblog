3c1e48375199c63ca83937ba33b5fb646b69a3d8
hyang <hymeldon@gmail.com>
2018-06-29 11:37:14 -0400

    Add toolbar to test the material module.

--- client/src/app/components/site/index.component.ts
+++ client/src/app/components/site/index.component.ts
@@ -2,7 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-site',
-  template: '<router-outlet></router-outlet>',
+  template: `
+<div>
+<mat-toolbar color="primary">
+<mat-toolbar-row>
+<span>Toolbar</span>
+</mat-toolbar-row>
+</mat-toolbar>
+</div>
+<router-outlet></router-outlet>
+`,
 })
 
 export class IndexComponent {
