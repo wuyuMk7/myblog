93fe2aab3f242bd1a91ac5bd62c667f0e6bdad68
hyang <hymeldon@gmail.com>
2018-10-14 16:22:34 -0400

    feat(database): add database connection to mongodb

--- server/server.js
+++ server/server.js
@@ -1,22 +1,73 @@
+'use strict';
+
 const Koa = require('koa');
 const app = new Koa();
 
-const serve = require('koa-static');
+const serve = require('koa-static'),
+      bodyParser = require('koa-bodyparser'),
+      mongo = require('koa-mongo');
+
+const config = require('./config');
+const routers = require('./routers/index.router');
 
 const staticFiles = __dirname + '/../public/dist';
 const opts = {};
 
-if (app.env != 'production') {
+if (app.env != 'production' && app.env != 'backend_dev') {
     const koaWebpack = require('koa-webpack');
     const historyFallback = require('koa2-history-api-fallback');
     const webpackConfig = require('../client/config/webpack.dev');
     
-    koaWebpack({ config: webpackConfig, devMiddleware: { publicPath: '/' }, hotClient: { hmr: false, reload: true } })
+    koaWebpack(
+        {
+            config: webpackConfig,
+            devMiddleware: { publicPath: '/' }, hotClient: { hmr: false, reload: true }
+        })
         .then((middleware) => {
             app.use(historyFallback());
             app.use(middleware);
         });
 }
+
+const database = ((config) => {
+    let env = (app.env == 'frontend_dev' || app.env == 'backend_dev') ? 'development' : app.env;
+    let c = config.database[env];
+    let db = c.db || 'test_blog',
+        port = c.port || '27017',
+        host = c.host.join(`:${port},`) || 'localhost',
+        user = c.user || '',
+        pwd = c.pwd || '',
+        extra = c.extra || '';
+
+    return {
+        "db": db, "port": port, "host": host,
+        "user": user, "pwd": pwd, "extra": extra
+    };
+})(config);
+const databaseUrl = ((db) => {
+    let userString = '';
+    if (db.user != '' && db.pwd != '')
+        userString = `${db.user}:${db.pwd}@`;
+
+    let url = `mongodb://${userString}${db.host}:${db.port}/${db.db}`;
+    if (db.extra != '')
+        url += `?${db.extra}`;
+
+    return url;
+})(database);
+
+app.use(mongo({
+    uri: databaseUrl,
+    max: 100,
+    min: 1
+}));
+app.use(async (ctx, next) => {
+    ctx.db = ctx.mongo.db(database.db);
+    await next();
+});
+
+app.use(bodyParser());
+app.use(routers.routes());
 
 //app.use(require('koa-static')(staticFiles, opts));
 app.listen(4000);
