f48e86e5deb62cc0c9644069e443cb52f55ebb85
hyang <hymeldon@gmail.com>
2018-11-23 16:36:53 -0500

    feat(siteinfo): add site.js to store site related information

---
+++ server/site.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var site = {
+    
+};
+
+var admin = {
+    username: "test_admin",
+    password: "118ae1503960f27133f62e1c6dd5648b3f1ec964590e327d6e03786098e51044"
+};
+
+module.exports.site = site;
+module.exports.admin = admin;
