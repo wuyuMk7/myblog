8a3a75652a2c4b405e13d6c15787973198d4f6dd
hyang <hymeldon@gmail.com>
2019-03-10 17:11:53 -0400

    feat(post): add content render (md&katex) to post component
    Add member variable 'postContent' to post component and page.
    Add 'marked' and 'katex' to post model to render content.
    Add member method 'renderContent' to post model.
    Add new test post data to post service.

--- client/src/app/models/post.ts
+++ client/src/app/models/post.ts
@@ -1,4 +1,6 @@
 import * as moment from 'moment';
+import * as marked from 'marked';
+import * as katex from 'katex';
 
 import { Comment } from './comment';
 
@@ -16,5 +18,49 @@ export class Post {
   commentCount: number;
   like: number;
 
-  constructor() {}
+  constructor(postInfo:any) {
+    this.id = postInfo.id;
+    this.title = postInfo.title;
+    this.url = postInfo.url;
+    this.tags = postInfo.tags;
+    this.desc = postInfo.desc;
+    this.content = postInfo.content;
+    this.comments = postInfo.comments;
+    this.createdAt = postInfo.createdAt;
+    this.viewCount = postInfo.viewCount;
+    this.commentCount = postInfo.commentCount;
+    this.like = postInfo.like;
+  }
+
+  renderContent(): string {
+    let content:string;
+
+    let formulaIndicatorLength = 9;
+    let leftFormulaIndicator = "<$formula";
+    let rightFormulaIndicator = "formula$>";
+
+    content = this.content;
+
+    let index = 0;
+    let contentLength = content.length;
+    while(index + formulaIndicatorLength * 2<= contentLength) {
+      let leftIndex = content.indexOf(leftFormulaIndicator, index);
+      if (leftIndex == -1) break;
+
+      let rightIndex = content.indexOf(rightFormulaIndicator, leftIndex + formulaIndicatorLength);
+      if (rightIndex == -1) break;
+
+      let formula = content.slice(leftIndex + formulaIndicatorLength, rightIndex);
+      if (formula.length != 0)
+        formula = "<div class=\"in-post-formula\">" + katex.renderToString(formula) + "</div>";
+
+      content = content.substr(0, leftIndex) + formula + content.substr(rightIndex + formulaIndicatorLength);
+
+      index = leftIndex + formula.length;
+    }
+
+    content = marked(content);
+
+    return content;
+  }
 }
